b66ee46f7503d6e6714319868cd211f65920fd1e
AlefMemTav <190115548@aluno.unb.br>
2022-07-14 22:07:49 -0300

    Created Home.module.css and improving Home page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Projects from './components/pages/Projects'
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <Router>
