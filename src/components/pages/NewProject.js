9bdfcdec5785f8759226d844f75b1a9374dd326b
AlefMemTav <190115548@aluno.unb.br>
2022-07-16 20:51:54 -0300

    Created NewProject.module.css and updated NewProject

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,13 @@
+import styles from './NewProject.module.css'
+
 function NewProject(){
-    return <h1>New Project</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie o seu projeto e adicione os serviços</p>
+            <p>Formulário</p>
+        </div>
+    )
 }
 
 export default NewProject
