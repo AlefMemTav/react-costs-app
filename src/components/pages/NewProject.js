b04f62b1e69eb33947ce3cb5de3904a8528248ec
AlefMemTav <190115548@aluno.unb.br>
2022-07-16 21:14:15 -0300

    Created ProjectForm

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,3 +1,5 @@
+import ProjectForm from '../project/ProjectForm'
+
 import styles from './NewProject.module.css'
 
 function NewProject(){
@@ -5,7 +7,7 @@ function NewProject(){
         <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
             <p>Crie o seu projeto e adicione os serviços</p>
-            <p>Formulário</p>
+            <ProjectForm />
         </div>
     )
 }
